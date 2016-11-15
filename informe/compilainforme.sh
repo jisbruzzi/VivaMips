44944390305d62b401f226b3832ecf3e6943c3f2
jose <jose@jose-HP-Pavilion-dv6-Notebook-PC.(none)>
2016-11-15 11:52:54 -0300

    Agrego el informe del TP1 para modificarlo

---
+++ informe/compilainforme.sh
@@ -0,0 +1,8 @@
+latex informe.tex
+dvipdfm informe.dvi
+rm informe.log
+rm informe.dvi
+rm informe.aux
+echo ""
+echo ""
+echo "Esto tb vuela los logs y toda esa basura (ojo si necesitas mirar los logs)"
